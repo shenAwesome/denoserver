141b3efdb9b35581d5cf027db3932aafb67a5c8d
Shen Z <shenAwesome@gmail.com>
2022-02-21 17:08:57 +1100

    gogo

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,27 @@
 // deno-lint-ignore-file require-await no-unused-vars
 import { v4 } from "https://deno.land/std@0.126.0/uuid/mod.ts";
-import { ld as _ } from "https://x.nest.land/deno-lodash@1.0.0/mod.ts";
+import { ld as _ } from "https://arweave.net/L92fXEGOMl2rGqlDvH6fvtEORNo2LlbDkvtn3jik9F4/mod.ts";
 import { request } from 'https://cdn.skypack.dev/@esri/arcgis-rest-request?dts';
 import { UserSession } from 'https://cdn.skypack.dev/@esri/arcgis-rest-auth';
-import { getLayer } from 'https://cdn.skypack.dev/@esri/arcgis-rest-feature-layer'; //?dts will fail to compile
+import { getLayer } from 'https://cdn.skypack.dev/@esri/arcgis-rest-feature-layer'; //?dts will fail to compile 
+import { toGeom } from "https://esm.sh/@aslab/geocalc@1.0.32"
+//import jsts from 'https://cdn.skypack.dev/jsts';
 
 class Test {
+
+    testGeom(radius = 100) {
+        const point = toGeom(`
+         {
+            "type": "Point",
+            "coordinates": [125.6, 10.1]
+          } 
+        `)
+        const polygon = point.buffer(parseInt(radius + ''))
+        return {
+            test: 'hello',
+            feat: polygon.area()
+        }
+    }
 
     async test(name: string, age: string) {
         name = _.upperCase(name)
@@ -15,13 +31,12 @@ class Test {
             url: "https://sampleserver6.arcgisonline.com/arcgis/rest/services/ServiceRequest/FeatureServer/0"
         })
 
-        console.log(JSON.stringify(layer))
-
         return {
             ret: name + age,
             time: Date.now(),
             esri,
-            layer: layer.type
+            layer: layer.type,
+            version: 1.0
         };
     }
 
